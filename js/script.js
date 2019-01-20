d3f920c4f05a11150f51d63e778aa334682284b2
ec17531 <markb1994@icloud.com>
2019-01-20 21:21:13 +0000

    All photos images and css done

--- js/script.js
+++ js/script.js
@@ -12,7 +12,42 @@ function init() {
     $('#maestroTile').click( function() {
         linkTo('maestro');
     });
+
+    $('#footerAdam').click( function() {
+        linkTo('adam');
+    });
+
+    $('#footerMaestro').click( function() {
+        linkTo('maestro');
+    });
+
+    $('#footerTravelcast').click( function() {
+        linkTo('travelcast');
+    });
+
+    // Activate Carousel
+    $("#myCarousel").carousel();
+        
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+        
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
 }
+
 
 
 function linkTo(page) {
